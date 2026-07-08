95e843faaba967525392d1a386fdab9ae55b66d0
Agash2110 <agash211099@gmail.com>
2026-07-08 12:00:40 +0530

    Add files via upload

---
+++ day2.js
@@ -0,0 +1,22 @@
+
+
+
+var fristName ="Agash"
+console.log(fristName)
+console.log(typeof fristName)
+
+var companyName ="Yitro"
+console.log(companyName)
+console.log(typeof companyName)
+
+var mobileNumber =7890373892
+console.log(mobileNumber)
+console.log(typeof mobileNumber)
+
+var isAutomation = true
+console.log(isAutomation)
+console.log(typeof isAutomation)
+
+var hasPlaywright
+console.log(hasPlaywright)
+console.log(typeof hasPlaywright)
